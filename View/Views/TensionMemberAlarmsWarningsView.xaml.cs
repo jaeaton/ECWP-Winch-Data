9bf4557e8c02d3ce1702762b008fa544b5d00eae
Josh <jeaton@whoi.edu>
2022-01-26 20:06:39 -0500

    Added Tension Member Alarms and Warning View along with LED control (Modified Radio Button)

---
+++ View/Views/TensionMemberAlarmsWarningsView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Views
+{
+    /// <summary>
+    /// Interaction logic for TensionMemberAlarmsWarningsView.xaml
+    /// </summary>
+    public partial class TensionMemberAlarmsWarningsView : UserControl
+    {
+        public TensionMemberAlarmsWarningsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
