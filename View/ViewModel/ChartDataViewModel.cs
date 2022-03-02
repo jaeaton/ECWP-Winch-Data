b8b167e109027b14942a192ff5e1d46df875cde8
Josh <jeaton@whoi.edu>
2022-03-02 16:32:11 -0500

    Updated chart to scale from zero

--- View/ViewModel/ChartDataViewModel.cs
+++ View/ViewModel/ChartDataViewModel.cs
@@ -5,6 +5,8 @@
         //public static ObservableCollection<ObservablePoint> _observableValues = new ObservableCollection<ObservablePoint>();
         public static ObservableCollection<DateTimePoint> _observableValues = new ObservableCollection<DateTimePoint>();
         public static ObservableCollection<ISeries> Series { get; set; }
+        public static ObservableCollection<DateTimePoint> _observableValuesZero = new ObservableCollection<DateTimePoint>();
+        //public static ObservableCollection<ISeries> SeriesZero { get; set; }
         public static IEnumerable<ICartesianAxis> XAxes { get; set; }
         public static IEnumerable<ICartesianAxis> YAxes { get; set; }
 
@@ -16,16 +18,26 @@
             Series = new ObservableCollection<ISeries>
             {
                 new LineSeries<DateTimePoint>
-                //new LineSeries<ObservablePoint>
                 {
                     Values = _observableValues,
                     Fill = null,
                     GeometrySize = 0,
                     LineSmoothness = 0,
-                    Stroke = new SolidColorPaint(SKColors.CornflowerBlue, 1),
-                                       
+                    Stroke = new SolidColorPaint(SKColors.CornflowerBlue, 1)
+
+                },
+                new LineSeries<DateTimePoint>
+                {
+                    Values = _observableValuesZero,
+                    Fill = null,
+                    GeometrySize = 0,
+                    LineSmoothness = 0,
+                    Stroke = new SolidColorPaint(SKColors.LightSlateGray, 1),
+
                 }
+
             };
+            
             XAxes = new List<Axis>
             {
                 new Axis
@@ -41,6 +53,7 @@
             {
                 new Axis
                 {
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray)
                     //MinLimit = 0,
                     //MaxLimit = (DataHandlingViewModel.maxData.MaxTension.Tension + 100)
                     //MaxLimit = 6000
@@ -58,10 +71,12 @@
             DateTime dateTime = DateTime.ParseExact($"{ latest.Date } { latest.Time }","yyyyMMdd HH:mm:ss.fff", provider);
             //double.TryParse(latest.Tension, out double Tension);
             _observableValues.Add(new DateTimePoint { DateTime = dateTime, Value = latest.Tension });
+            _observableValuesZero.Add(new DateTimePoint { DateTime = dateTime, Value = 0 });
             //_observableValues.Add(new ObservablePoint { X = i++, Y = latest.Tension });
             if (_observableValues.Count > 500)
             {
                 _observableValues.RemoveAt(0);
+                _observableValuesZero.RemoveAt(0);
             }
         }
 
