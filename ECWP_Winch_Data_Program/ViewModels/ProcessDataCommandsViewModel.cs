28aea0aea8d26ccef9185d1ad1b111887dd4cdee
Joshua Eaton <jeaton@whoi.edu>
2024-03-06 09:01:25 -0500

    Update process data commands wire up log processing view

--- ECWP_Winch_Data_Program/ViewModels/ProcessDataCommandsViewModel.cs
+++ ECWP_Winch_Data_Program/ViewModels/ProcessDataCommandsViewModel.cs
@@ -83,9 +83,55 @@
 
         [RelayCommand]
         private void SingleProcessFiles() 
-        { 
+        {
+            ExcelViewModel.SetWireLogFileName();
+            
             ProcessDataReadFilesViewModel.ReadDataFiles(ProcessDataViewModel.ParseData);
         
+        }
+
+        private void FindFiles()
+        {
+            ConfigDataStore _config = MainViewModel._configDataStore;
+            ParseDataStore ParseData = ProcessDataViewModel.ParseData;
+
+            ParseData.FileList = new Store.SortableObservableCollection<string>();//SortableObservableCollection<string>();
+            DirectoryInfo di = new DirectoryInfo(_config.CurrentWinch.RawLogDirectory);
+
+            ParseData.FileList.Clear();
+            string extension;
+            //Set File extension
+            switch (_config.CurrentWinch.typ)
+            {
+                case "SIO Traction Winch":
+                    extension = "*.Raw";
+                    break;
+                case "MASH Winch":
+                    extension = "*.CSV";
+                    break;
+                case "Armstrong CAST 6":
+                    extension = "*.MTN_WINCH";
+                    break;
+                case "UNOLS String":
+                    extension = "*.wire.Log";
+                    break;
+                case "Jay Jay":
+                    extension = "*.CSV";
+                    break;
+                default:
+                    extension = string.Empty;
+                    break;
+            }
+            //Search for files and andd them to the list
+            if (extension != string.Empty)
+            {
+                foreach (var fi in di.GetFiles(extension, SearchOption.AllDirectories))
+                {
+                    ParseData.FileList.Add($"{fi.DirectoryName}\\{fi.Name}");//fi.Name);
+                }
+                ParseData.NumberOfFiles = ParseData.FileList.Count;
+                //ParseData.FileList;//Sort();
+            }
         }
     }
 }
