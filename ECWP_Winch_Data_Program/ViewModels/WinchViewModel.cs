19bd4fcba651ea59a713fcc3738481c4e3530996
Joshua Eaton <jeaton@whoi.edu>
2024-04-22 11:18:40 -0400

    Commented out save file command

--- ECWP_Winch_Data_Program/ViewModels/WinchViewModel.cs
+++ ECWP_Winch_Data_Program/ViewModels/WinchViewModel.cs
@@ -369,7 +369,36 @@ namespace ViewModels
             }
             
         }
+        //[RelayCommand]
+        //private async Task SaveFile()
+        //{
+        //    ErrorMessages?.Clear();
+        //    try
+        //    {
+        //        var filesService = App.Current?.Services?.GetService<IFilesService>();
+        //        if (filesService is null) throw new NullReferenceException("Missing File Service instance.");
 
-       
+        //        var file = await filesService.SaveFileAsync();
+        //        if (file is null) return;
+
+
+        //        // Limit the text file to 1MB so that the demo wont lag.
+        //        if (FileText?.Length <= 1024 * 1024 * 1)
+        //        {
+        //            var stream = new MemoryStream(Encoding.Default.GetBytes((string)FileText));
+        //            await using var writeStream = await file.OpenWriteAsync();
+        //            await stream.CopyToAsync(writeStream);
+        //        }
+        //        else
+        //        {
+        //            throw new Exception("File exceeded 1MB limit.");
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        ErrorMessages?.Add(e.Message);
+        //    }
+        //}
+
     }
 }
