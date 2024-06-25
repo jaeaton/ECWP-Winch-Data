c316a60339b7e8e5b6b474177a460094348f35b7
Joshua Eaton <jeaton@whoi.edu>
2024-06-25 14:56:19 -0400

    Update Add/;remove winch buttons

---
+++ ECWP_Winch_Data_Program/Views/WinchDetailAddRemoveView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Views
+{
+    public partial class WinchDetailAddRemoveView : UserControl
+    {
+        public WinchDetailAddRemoveView()
+        {
+            InitializeComponent();
+        }
+    }
+}
