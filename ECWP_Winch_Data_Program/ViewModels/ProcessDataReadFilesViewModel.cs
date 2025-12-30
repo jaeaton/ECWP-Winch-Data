41b354e0a766fd9331e6bbcf6a56fc6005898e3b
Joshua Eaton <jeaton@whoi.edu>
2025-12-30 13:24:38 -0500

    package updates store data for plotting from processing very slow

--- ECWP_Winch_Data_Program/ViewModels/ProcessDataReadFilesViewModel.cs
+++ ECWP_Winch_Data_Program/ViewModels/ProcessDataReadFilesViewModel.cs
@@ -45,6 +45,7 @@ namespace ViewModels
         public static void ReadDataFromLogs(ParseDataStore parseData)
         {
             string filePath = parseData.Directory;
+            parseData.DataToPlot.Clear();
             foreach (var fin in parseData.FileList)
             {
                 ProcessDataViewModel.ParseData.NumberOfProcessedFiles++;
@@ -378,6 +379,12 @@ namespace ViewModels
                     ProcessDataViewModel.ParseData.CancellationTokenSource.Token.ThrowIfCancellationRequested();
                 }
             }
+            //Plot Data
+            while (parseData.DataToPlot.Count > 0)
+            {
+                parseData.ChartData.AddData(parseData.DataToPlot[0]);
+                parseData.DataToPlot.RemoveAt(0);
+            }
         }
 
         //Step 3
@@ -481,10 +488,10 @@ namespace ViewModels
                     //i = 0;
                 }
 
-                //if (castActive)
-                //{
-                //    parseData.DataToPlot.Add(lineData);
-                //}
+                if (castActive)
+                {
+                    parseData.DataToPlot.Add(lineData);
+                }
             }
             //Plot Data
             //foreach (var val in parseData.DataToPlot)
